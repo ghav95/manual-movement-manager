be8c221c0d45d24db05bc9d4d664d4c5298b9c25
ghav95 <ghav95@gmail.com>
2024-11-23 01:30:31 -0300

    Build: Added IntegrationTests project

---
+++ backend/Manual.Movement.Manager/tests/Manual.Movement.Manager.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Manual.Movement.Manager.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Manual.Movement.Manager.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7c9315e-7739-4914-9169-d5b591159d05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
