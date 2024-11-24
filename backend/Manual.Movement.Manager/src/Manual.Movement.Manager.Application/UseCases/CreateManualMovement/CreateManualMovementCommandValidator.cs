e37ed1b219788785df58ec618aee389fc982a9c3
ghav95 <ghav95@gmail.com>
2024-11-24 17:11:55 -0300

    Feat: Add RoutePrefix for the controller and documentation

--- backend/Manual.Movement.Manager/src/Manual.Movement.Manager.Application/UseCases/CreateManualMovement/CreateManualMovementCommandValidator.cs
+++ backend/Manual.Movement.Manager/src/Manual.Movement.Manager.Application/UseCases/CreateManualMovement/CreateManualMovementCommandValidator.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace Manual.Movement.Manager.Application.UseCases.CreateManualMovement
 {
-    internal class CreateManualMovementCommandValidator
+    public class CreateManualMovementCommandValidator : AbstractValidator<CreateManualMovementCommand>
     {
+        public CreateManualMovementCommandValidator()
+        {
+            RuleFor(x => x.Month)
+                .NotEmpty()
+                .WithMessage("Month cannot be null or empty.");
+
+            RuleFor(x => x.Year)
+                .NotEmpty()
+                .WithMessage("Year cannot be null or empty.");
+
+            RuleFor(x => x.ProductId)
+                .NotEmpty()
+                .WithMessage("ProductId cannot be null or empty.");
+
+            RuleFor(x => x.CosifId)
+                .NotEmpty()
+                .WithMessage("CosifId cannot be null or empty.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("Description cannot be null or empty.");
+
+            RuleFor(x => x.Value)
+                .NotEmpty()
+                .WithMessage("Value cannot be null or empty.");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UserId cannot be null or empty.");
+        }
     }
 }
--- backend/Manual.Movement.Manager/src/Manual.Movement.Manager.WebApi/Controllers/ProductController.cs
+++ backend/Manual.Movement.Manager/src/Manual.Movement.Manager.WebApi/Controllers/ProductController.cs
@@ -2,29 +2,42 @@
 using Manual.Movement.Manager.WebApi.Transport.GetAllProduct;
 using MediatR;
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Manual.Movement.Manager.WebApi.Controllers
 {
+    /// <summary>
+    /// Handles operations related to products.
+    /// </summary>
+    [RoutePrefix("api/v1/products")]
     public class ProductController : ApiController
     {
         private readonly IMediator _mediator;
 
-        public ProductController(IMediator mediator) 
+        public ProductController(IMediator mediator)
             => _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
 
-
-        public async Task<IHttpActionResult> Get(CancellationToken cancellationToken = default)
+        /// <summary>
+        /// Gets all products.
+        /// </summary>
+        /// <param name="cancellationToken">Token for cancelling the operation.</param>
+        /// <returns>A list of products or an error response.</returns>
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> Get(
+            CancellationToken cancellationToken = default)
         {
             var command = new GetAllProductCommand();
 
             var output = await _mediator.Send(command, cancellationToken)
                 .ConfigureAwait(false);
 
-            if(output != null) return Ok(output.MapToResponse());
-            return BadRequest();
+            if (output != null) return Ok(output.MapToResponse());
+
+            return Content(HttpStatusCode.BadRequest, "Failed to retrieve products.");
         }
     }
 }
