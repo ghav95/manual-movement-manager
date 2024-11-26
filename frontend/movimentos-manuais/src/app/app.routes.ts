a886ab398b36b88748886272e09a46e2aff76ade
ghav95 <ghav95@gmail.com>
2024-11-26 02:23:11 -0300

    Fix: route fix

--- frontend/movimentos-manuais/src/app/app.routes.ts
+++ frontend/movimentos-manuais/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { GridComponent } from './components/grid/grid.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'form', pathMatch: 'full' }
+  { path: '', component: FormComponent },
+  { path: '*', component: GridComponent },  // Default route
+  { path: '**', redirectTo: 'form' }            // Wildcard route for any unmatched routes, redirects to home (FormComponent)
 ];
